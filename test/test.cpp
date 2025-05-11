7494acc06e1d4bd1dc743770d299d1a5c04f200b
wsq <wsq1275093443@gmail.com>
2025-05-11 17:30:38 +0800

    add: CI/CD google test

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,10 @@
-//
-// Created by 王单强 on 25-5-11.
-//
+#include <gtest/gtest.h>
+
+TEST(SampleTest, Addition) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
